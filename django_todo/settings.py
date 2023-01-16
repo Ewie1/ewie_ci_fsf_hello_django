2bdbf287d097890ce295c84e6dbdb1670fb8f8db
Ewie1 <shephvo12@gmail.com>
2023-01-16 14:00:45 +0000

    Change degug to false

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n(w37gm_+c1fk5b0in&+sq+=rl*-v%k7_skgdyn*7gf_8hiy*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
